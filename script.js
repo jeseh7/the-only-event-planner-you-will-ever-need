5198b89639571e6bde2b72a1f3a11446de4b87ec
Jese H <pilers2000@hotmail.com>
2023-09-22 20:14:51 -0400

    fixed up the html

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var saveButtonEl = $( '.fa-save' );
-var eventEl = $( '#userInput' ).val();
+var mainEl = $( 'main' );
+
+console.log(mainEl.children[0]);
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
@@ -25,8 +27,10 @@ $(document).ready(function () {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
+  /*  for (let i = 0; i < mainEl.length; i++) {
+        const element = array[i]; 
         
-
+    }*/
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
